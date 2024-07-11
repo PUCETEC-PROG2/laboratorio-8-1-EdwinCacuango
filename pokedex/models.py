57720edccecbd107a1aa5ad0031304fd995aa00a
Edwin Cacuango <edwin-fernando@hotmail.es>
2024-07-11 18:41:31 -0500

    Modelos, migraciones, layout y uso de bootstrap builder - Creacion de modelo Pokemon - Configuracion de conexión a bd - Migracion de modelo - Envio de modelo a bd - Configuracion de admin para agregar datos - Cambios a view para extraer pokemos en ambas paginas - Creacion de layout para separacion de componentes repetidos - Reconfiguracion de templates para usar layout - Uso de bootstrap builder con tema predeterminado en archivo estático

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,24 @@
+
+
 from django.db import models
+from django.db.models.functions.text import Length
 
 # Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length = 30, null = False)
+
+    POKEMON_TYPES = [
+        ('A', 'Agua'),
+        ('E', 'Electrico'),
+        ('F', 'Fuego'),
+        ('L', 'Lagartija'),
+        ('P', 'Planta'),
+        ('T', 'Tierra'),
+    ]
+    type = models.CharField(max_length = 30, null = False, choices = POKEMON_TYPES)
+    weight = models.DecimalField(null = False, default = 1, max_digits= 4, decimal_places=2)
+    height = models.DecimalField(null = False, default = 1, max_digits= 4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from pokedex.models import Pokemon
+
 def index(request):
-    pokemons = ['charmander', 'pikachu', 'squirtle']
+    pokemons = Pokemon.objects.all()
     template = loader.get_template('index.html')
     return HttpResponse(template.render({'pokemons': pokemons}, request))
 
 def pokemon(request, pokemon):
     template = loader.get_template('display_pokemon.html')
+    pokemon = Pokemon.objects.get(name=pokemon)
+
     context = {
         'pokemon': pokemon
     }
